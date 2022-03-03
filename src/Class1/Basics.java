d54158197168a3e6e0c46088a0164e6087ae8f6d
Deepak Sharma <sharma.deepak8616@gmail.com>
2022-03-02 21:24:40 -0500

    Adding class1 code/notes

---
+++ src/Class1/Basics.java
@@ -0,0 +1,44 @@
+package Class1;
+
+public class Basics {
+
+
+    /**
+     * Entire project has to be error free in order to execute any file of the project.
+     *
+     * Compile Time error:
+     *      Any error indicated by java without execution.
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");      // sout statement
+        /**
+         * sout statement --> is used to print in the console.
+         *
+         * sout -> shortcut to generate System.out.println()
+         */
+
+        System.out.println("Have a good day to you sir ** ^^");
+
+    }
+
+    /**
+     * main method: starting point of the execution of any java-file/class
+     *
+     * shortcut to create main method : psvm (intelliJ)
+     *
+     * In a class, there will ALWAYS be only ONE main method.
+     *
+     * when we run main-method of a class,
+     *      code written inside the main-method will execute.
+     *      Any code written outside the main-method in the class will not execute.
+     */
+
+    /**
+     * Console window: part of intelliJ to view the results.
+     *
+     * print it or sout it or add sout statement
+     */
+
+}
---
+++ src/Class1/Comments.java
@@ -0,0 +1,29 @@
+package Class1;
+
+public class Comments {
+
+    // Note -> NO code outside the curly-braces of class
+    // After double-slash, everything on this line will be treated as comment.
+
+    /*
+        This is a
+
+        way to add
+
+        multi-line
+
+        comment.
+
+     *
+
+    /**
+     * This is another
+     *
+     * way to add
+     *
+     * multi line
+     *
+     * comment
+     */
+
+}
